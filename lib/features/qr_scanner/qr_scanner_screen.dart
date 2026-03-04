44c2e45dc917bae51f8d20e8c881e09b3e8fc2a6
kamilpasierski <pasierskidev@gmail.com>
2026-03-04 20:43:00 +0100

    feat: integrate mobile_scanner and configure native camera permissions

--- lib/features/qr_scanner/qr_scanner_screen.dart
+++ lib/features/qr_scanner/qr_scanner_screen.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:relay_app/core/theme/app_theme.dart';
 
-class QrScannerScreen extends StatelessWidget {
+class QrScannerScreen extends StatefulWidget {
   const QrScannerScreen({super.key});
+
+  @override
+  State<QrScannerScreen> createState() => _QrScannerScreenState();
+}
+
+class _QrScannerScreenState extends State<QrScannerScreen> {
+  final MobileScannerController _cameraController = MobileScannerController();
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +25,37 @@ class QrScannerScreen extends StatelessWidget {
         title: const Text('Detekcja QR'),
         backgroundColor: AppTheme.primaryDark,
         foregroundColor: AppTheme.surfaceWhite,
-      ),
-      body: Center(
-        
-            // TODO: Bindowanie natywnego interfejsu API kamery i dekodera strumienia
-            
-
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.flashlight_on),
+            onPressed: () => _cameraController.toggleTorch(),
           ),
-        );
+          IconButton(
+            icon: const Icon(Icons.cameraswitch),
+            onPressed: () => _cameraController.switchCamera(),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          MobileScanner(
+            controller: _cameraController,
+            onDetect: (capture) {
+              debugPrint('Zarejestrowano wektor wejściowy z matrycy optycznej');
+            },
+          ),
+          Center(
+            child: Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                border: Border.all(color: AppTheme.brandOrange, width: 3),
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
